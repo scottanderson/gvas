d60cc732063e958e5e28a89975dc572502094599
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2026-06-19 20:13:36 -0400

    USaveGame

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -22,6 +22,7 @@ mod f_text;
 mod f_text_property;
 mod t_array;
 mod t_optional;
+mod u_save_game;
 
 use binrw::binrw;
 
@@ -52,6 +53,7 @@ pub use crate::types::{
     f_text_property::FTextProperty,
     t_array::TArray,
     t_optional::TOptional,
+    u_save_game::USaveGame,
 };
 
 pub const NAME_NONE: &str = "None";
@@ -151,168 +153,3 @@ ue_struct!(FRotator, LWC, pitch, yaw, roll);
 ue_struct!(FTimespan, ticks: i64);
 ue_struct!(FVector, LWC, x, y, z);
 ue_struct!(FVector2D, x: f64, y: f64);
-
-#[binrw]
-#[brw(little)]
-#[derive(Debug)]
-pub struct SaveGameFile {
-    pub header: FSaveGameHeader,
-
-    #[brw(if(header.serialization_format().property_tag_complete_type_name))]
-    #[br(temp, assert(spacer == 0))]
-    #[bw(calc(0))]
-    spacer: u8,
-
-    #[brw(args(header.serialization_format()))]
-    pub properties: TaggedProperties,
-
-    #[br(temp, assert(footer == 0))]
-    #[bw(calc(0))]
-    footer: u32,
-}
-
-#[cfg(test)]
-mod test {
-    use std::{
-        fs::File,
-        io::{Cursor, Read, Seek},
-        path::Path,
-    };
-
-    use binrw::BinRead;
-
-    use crate::error::Result;
-    use crate::types::SaveGameFile;
-
-    fn test_save_game_file<P: AsRef<Path> + std::fmt::Debug>(path: P) -> Result<()> {
-        // Open
-        let mut file = File::open(&path)?;
-
-        // Read
-        let mut buf = Vec::new();
-        let len = file.read_to_end(&mut buf)?;
-
-        // Parse
-        let mut cursor = Cursor::new(buf);
-        let _result = SaveGameFile::read(&mut cursor)?;
-        assert_eq!(len as u64, cursor.stream_position()?);
-
-        // TODO: Write
-        // let mut buf2 = vec![0u8; len];
-        // let mut cursor2 = Cursor::new(buf2);
-        // SaveGameFile::write(&result, &mut cursor2)?;
-
-        // TODO: Compare
-        // let buf = cursor.into_inner();
-        // let buf2 = cursor2.into_inner();
-        // assert!(buf == buf2);
-
-        // Success
-        Ok(())
-    }
-
-    #[test]
-    fn test_regression_01_bin() -> Result<()> {
-        test_save_game_file("tests/resources/regression_01.bin")
-    }
-
-    #[test]
-    fn test_options_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Options.sav")
-    }
-
-    #[test]
-    fn test_enum_array_sav() -> Result<()> {
-        test_save_game_file("tests/resources/enum_array.sav")
-    }
-
-    #[test]
-    fn test_component8_sav() -> Result<()> {
-        test_save_game_file("tests/resources/component8.sav")
-    }
-
-    #[test]
-    fn test_complete_property_tag_sav() -> Result<()> {
-        test_save_game_file("tests/resources/complete_property_tag.sav")
-    }
-
-    #[test]
-    fn test_slot2_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Slot2.sav")
-    }
-
-    #[test]
-    fn test_features_01_bin() -> Result<()> {
-        test_save_game_file("tests/resources/features_01.bin")
-    }
-
-    #[test]
-    fn test_profile_0_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Profile_0.sav")
-    }
-
-    #[test]
-    fn test_save_slot_03_sav() -> Result<()> {
-        test_save_game_file("tests/resources/SaveSlot_03.sav")
-    }
-
-    #[test]
-    fn test_package_version_524_sav() -> Result<()> {
-        test_save_game_file("tests/resources/package_version_524.sav")
-    }
-
-    #[test]
-    fn test_vector2d_sav() -> Result<()> {
-        test_save_game_file("tests/resources/vector2d.sav")
-    }
-
-    #[test]
-    fn test_delegate_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Delegate.sav")
-    }
-
-    #[test]
-    fn test_slot3_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Slot3.sav")
-    }
-
-    #[test]
-    fn test_assert_failed_sav() -> Result<()> {
-        test_save_game_file("tests/resources/assert_failed.sav")
-    }
-
-    #[test]
-    fn test_slot1_sav() -> Result<()> {
-        test_save_game_file("tests/resources/Slot1.sav")
-    }
-
-    #[test]
-    fn test_tagcontainer_sav() -> Result<()> {
-        test_save_game_file("tests/resources/tagcontainer.sav")
-    }
-
-    #[test]
-    fn test_string_table_entry_sav() -> Result<()> {
-        test_save_game_file("tests/resources/string_table_entry.sav")
-    }
-
-    #[test]
-    fn test_package_version_525_sav() -> Result<()> {
-        test_save_game_file("tests/resources/package_version_525.sav")
-    }
-
-    #[test]
-    fn test_ro_64bit_fav_sav() -> Result<()> {
-        test_save_game_file("tests/resources/ro_64bit_fav.sav")
-    }
-
-    #[test]
-    fn test_transform_sav() -> Result<()> {
-        test_save_game_file("tests/resources/transform.sav")
-    }
-
-    #[test]
-    fn test_text_property_noarray_bin() -> Result<()> {
-        test_save_game_file("tests/resources/text_property_noarray.bin")
-    }
-}
---
+++ src/types/u_save_game.rs
@@ -0,0 +1,168 @@
+use binrw::binrw;
+
+use crate::types::{FSaveGameHeader, TaggedProperties};
+
+#[binrw]
+#[brw(little)]
+#[derive(Debug)]
+pub struct USaveGame {
+    pub header: FSaveGameHeader,
+
+    #[brw(if(header.serialization_format().property_tag_complete_type_name))]
+    #[br(temp, assert(spacer == 0))]
+    #[bw(calc(0))]
+    spacer: u8,
+
+    #[brw(args(header.serialization_format()))]
+    pub properties: TaggedProperties,
+
+    #[br(temp, assert(footer == 0))]
+    #[bw(calc(0))]
+    footer: u32,
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        fs::File,
+        io::{Cursor, Read, Seek},
+        path::Path,
+    };
+
+    use binrw::BinRead;
+
+    use crate::error::Result;
+    use crate::types::USaveGame;
+
+    fn test_save_game_file<P: AsRef<Path> + std::fmt::Debug>(path: P) -> Result<()> {
+        // Open
+        let mut file = File::open(&path)?;
+
+        // Read
+        let mut buf = Vec::new();
+        let len = file.read_to_end(&mut buf)?;
+
+        // Parse
+        let mut cursor = Cursor::new(buf);
+        let _result = USaveGame::read(&mut cursor)?;
+        assert_eq!(len as u64, cursor.stream_position()?);
+
+        // TODO: Write
+        // let buf2 = vec![0u8; len];
+        // let mut cursor2 = Cursor::new(buf2);
+        // USaveGame::write(&result, &mut cursor2)?;
+
+        // TODO: Compare
+        // let buf = cursor.into_inner();
+        // let buf2 = cursor2.into_inner();
+        // assert!(buf == buf2);
+
+        // Success
+        Ok(())
+    }
+
+    #[test]
+    fn test_regression_01_bin() -> Result<()> {
+        test_save_game_file("tests/resources/regression_01.bin")
+    }
+
+    #[test]
+    fn test_options_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Options.sav")
+    }
+
+    #[test]
+    fn test_enum_array_sav() -> Result<()> {
+        test_save_game_file("tests/resources/enum_array.sav")
+    }
+
+    #[test]
+    fn test_component8_sav() -> Result<()> {
+        test_save_game_file("tests/resources/component8.sav")
+    }
+
+    #[test]
+    fn test_complete_property_tag_sav() -> Result<()> {
+        test_save_game_file("tests/resources/complete_property_tag.sav")
+    }
+
+    #[test]
+    fn test_slot2_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Slot2.sav")
+    }
+
+    #[test]
+    fn test_features_01_bin() -> Result<()> {
+        test_save_game_file("tests/resources/features_01.bin")
+    }
+
+    #[test]
+    fn test_profile_0_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Profile_0.sav")
+    }
+
+    #[test]
+    fn test_save_slot_03_sav() -> Result<()> {
+        test_save_game_file("tests/resources/SaveSlot_03.sav")
+    }
+
+    #[test]
+    fn test_package_version_524_sav() -> Result<()> {
+        test_save_game_file("tests/resources/package_version_524.sav")
+    }
+
+    #[test]
+    fn test_vector2d_sav() -> Result<()> {
+        test_save_game_file("tests/resources/vector2d.sav")
+    }
+
+    #[test]
+    fn test_delegate_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Delegate.sav")
+    }
+
+    #[test]
+    fn test_slot3_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Slot3.sav")
+    }
+
+    #[test]
+    fn test_assert_failed_sav() -> Result<()> {
+        test_save_game_file("tests/resources/assert_failed.sav")
+    }
+
+    #[test]
+    fn test_slot1_sav() -> Result<()> {
+        test_save_game_file("tests/resources/Slot1.sav")
+    }
+
+    #[test]
+    fn test_tagcontainer_sav() -> Result<()> {
+        test_save_game_file("tests/resources/tagcontainer.sav")
+    }
+
+    #[test]
+    fn test_string_table_entry_sav() -> Result<()> {
+        test_save_game_file("tests/resources/string_table_entry.sav")
+    }
+
+    #[test]
+    fn test_package_version_525_sav() -> Result<()> {
+        test_save_game_file("tests/resources/package_version_525.sav")
+    }
+
+    #[test]
+    fn test_ro_64bit_fav_sav() -> Result<()> {
+        test_save_game_file("tests/resources/ro_64bit_fav.sav")
+    }
+
+    #[test]
+    fn test_transform_sav() -> Result<()> {
+        test_save_game_file("tests/resources/transform.sav")
+    }
+
+    #[test]
+    fn test_text_property_noarray_bin() -> Result<()> {
+        test_save_game_file("tests/resources/text_property_noarray.bin")
+    }
+}
