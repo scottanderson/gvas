d2170b926fe7533a2dcf13a252c796bf1401332f
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2026-06-25 02:03:26 -0400

    Derive FBoolProperty

--- src/types/f_bool_property.rs
+++ src/types/f_bool_property.rs
@@ -2,23 +2,14 @@ use binrw::BinRead;
 
 use crate::types::{CollectionProperties, PropertyType};
 
-#[derive(Debug)]
-pub struct FBoolProperty(pub bool);
-
-impl BinRead for FBoolProperty {
-    type Args<'a> = (&'a PropertyType,);
-
-    fn read_options<R: std::io::Read + std::io::Seek>(
-        _reader: &mut R,
-        _endian: binrw::Endian,
-        (t,): Self::Args<'_>,
-    ) -> binrw::BinResult<Self> {
-        match t {
-            PropertyType::Incomplete {
+impl PropertyType {
+    fn bool_value(&self) -> binrw::BinResult<bool> {
+        match self {
+            Self::Incomplete {
                 extra: CollectionProperties::Bool { value },
                 ..
-            } => Ok(Self(*value != 0)),
-            PropertyType::Complete { flags, .. } => Ok(Self(flags.bool_true())),
+            } => Ok(*value != 0),
+            Self::Complete { flags, .. } => Ok(flags.bool_true()),
             _ => Err(binrw::Error::AssertFail {
                 pos: 0,
                 message: "BoolProperty type not found".to_string(),
@@ -26,3 +17,7 @@ impl BinRead for FBoolProperty {
         }
     }
 }
+
+#[derive(BinRead, Debug)]
+#[br(import(t: &PropertyType))]
+pub struct FBoolProperty(#[br(calc = t.bool_value()?)] pub bool);
